074918eaffbe2235baf37d1c5172b1702be44393
Tyler Ahlstrom <tylerahlstrom@gmail.com>
2024-07-02 09:01:52 -0500

    working version of box64

---
+++ box64/src/box64version.h
@@ -0,0 +1,8 @@
+#ifndef __BOX64_VERSION_H_
+#define __BOX64_VERSION_H_
+
+#define BOX64_MAJOR 0
+#define BOX64_MINOR 2
+#define BOX64_REVISION 9
+
+#endif //__BOX64_VERSION_H_
---
+++ box64/src/include/my_cpuid.h
@@ -0,0 +1,9 @@
+#ifndef __MY_CPUID_H__
+#define __MY_CPUID_H__
+#include <stdint.h>
+typedef struct x64emu_s x64emu_t;
+
+void my_cpuid(x64emu_t* emu, uint32_t tmp32u);
+uint32_t helper_getcpu(x64emu_t* emu);  // get the numa/cpu id actually running
+
+#endif //__MY_CPUID_H__
